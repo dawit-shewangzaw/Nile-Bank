3305c29a192d2c159b501db7fde37b5aaf2dd634
Dave051621 <dave051621@gmail.com>
2024-02-26 16:35:08 +0300

    Final-Project

---
+++ src/components/CardDeal.jsx
@@ -0,0 +1,25 @@
+import { card } from "../assets";
+import styles, { layout } from "../style";
+import Button from "./Button";
+
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Find a better card deal <br className="sm:block hidden" /> in few easy
+        steps.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+      Effortless comparison, instant rewards. Find the card that unlocks savings and benefits you truly deserve.
+      </p>
+
+      <Button styles={`mt-10`} />
+    </div>
+
+    <div className={layout.sectionImg}>
+      <img src={card} alt="billing" className="w-[100%] h-[100%]" />
+    </div>
+  </section>
+);
+
+export default CardDeal;
---
+++ src/constants/index.js
@@ -0,0 +1,197 @@
+import { people01, people02, people03, facebook, instagram, linkedin, twitter, airbnb, binance, coinbase, dropbox, send, shield, star  } from "../assets";
+
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "features",
+    title: "Features",
+  },
+  {
+    id: "product",
+    title: "Product",
+  },
+  {
+    id: "clients",
+    title: "Clients",
+  },
+];
+
+export const features = [
+  {
+    id: "feature-1",
+    icon: star,
+    title: "Rewards",
+    content:
+      "The best credit cards offer some tantalizing combinations of promotions and prizes",
+  },
+  {
+    id: "feature-2",
+    icon: shield,
+    title: "100% Secured",
+    content:
+      "We take proactive steps make sure your information and transactions are secure.",
+  },
+  {
+    id: "feature-3",
+    icon: send,
+    title: "Balance Transfer",
+    content:
+      "A balance transfer credit card can save you a lot of money in interest charges.",
+  },
+];
+
+export const feedback = [
+  {
+    id: "feedback-1",
+    content:
+      "Switching to this bank was the best decision! Their online tools are top-notch, making managing my finances a breeze. Plus, the customer service is always friendly and helpful.",
+    name: "Helen Alemu",
+    title: "Customer",
+    img: people01,
+  },
+  {
+    id: "feedback-2",
+    content:
+      "I finally feel like my bank understands my needs. They helped me find the perfect loan for my business and offered personalized advice every step of the way. Truly grateful!",
+    name: "Dr Alemayhu Derje",
+    title: "Customer",
+    img: people02,
+  },
+  {
+    id: "feedback-3",
+    content:
+      "This bank goes above and beyond! Not only do they offer competitive rates, but they also actively participate in community events and support local businesses. I'm proud to be a customer.",
+    name: "Henok Hagos",
+    title: "Customer",
+    img: people03,
+  },
+];
+
+export const stats = [
+  {
+    id: "stats-1",
+    title: "User Active",
+    value: "100,000+",
+  },
+  {
+    id: "stats-2",
+    title: "Trusted by Company",
+    value: "200+",
+  },
+  {
+    id: "stats-3",
+    title: "Transaction",
+    value: "$200M+",
+  },
+];
+
+export const footerLinks = [
+  {
+    title: "Useful Links",
+    links: [
+      {
+        name: "Content",
+        link: "https://www.hoobanknilebank.com/content/",
+      },
+      {
+        name: "How it Works",
+        link: "https://www.nilebank.com/how-it-works/",
+      },
+      {
+        name: "Create",
+        link: "https://www.nilebank.com/create/",
+      },
+      {
+        name: "Explore",
+        link: "https://www.nilebank.com/explore/",
+      },
+      {
+        name: "Terms & Services",
+        link: "https://www.nilebank.com/terms-and-services/",
+      },
+    ],
+  },
+  {
+    title: "Community",
+    links: [
+      {
+        name: "Help Center",
+        link: "https://www.nilebank.com/help-center/",
+      },
+      {
+        name: "Partners",
+        link: "https://www.nilebank.com/partners/",
+      },
+      {
+        name: "Suggestions",
+        link: "https://www.nilebank.com/suggestions/",
+      },
+      {
+        name: "Blog",
+        link: "https://www.nilebank.com/blog/",
+      },
+      {
+        name: "Newsletters",
+        link: "https://www.nilebank.com/newsletters/",
+      },
+    ],
+  },
+  {
+    title: "Partner",
+    links: [
+      {
+        name: "Our Partner",
+        link: "https://www.nilebank.com/our-partner/",
+      },
+      {
+        name: "Become a Partner",
+        link: "https://www.nilebank.com/become-a-partner/",
+      },
+    ],
+  },
+];
+
+export const socialMedia = [
+  {
+    id: "social-media-1",
+    icon: instagram,
+    link: "https://www.instagram.com/",
+  },
+  {
+    id: "social-media-2",
+    icon: facebook,
+    link: "https://www.facebook.com/",
+  },
+  {
+    id: "social-media-3",
+    icon: twitter,
+    link: "https://www.twitter.com/",
+  },
+  {
+    id: "social-media-4",
+    icon: linkedin,
+    link: "https://www.linkedin.com/",
+  },
+];
+
+export const clients = [
+  {
+    id: "client-1",
+    logo: airbnb,
+  },
+  {
+    id: "client-2",
+    logo: binance,
+  },
+  {
+    id: "client-3",
+    logo: coinbase,
+  },
+  {
+    id: "client-4",
+    logo: dropbox,
+  },
+];
